9d6712760ed862458309d3a624a9022f92c06689
gilbastidas <gilayala@Gilbertos-MacBook-Pro-4.local>
2016-11-08 15:59:06 -0600

    Static graph
    A static graph is created, it needs to show dynamic content. The number
    of relations are showed also.

--- semanticquery/views.py
+++ semanticquery/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from semanticquery.forms import SearchForm
 from django.http import HttpResponseRedirect
 from SPARQLWrapper import SPARQLWrapper, JSON
+from django.utils.safestring import mark_safe
 import json
 
 
@@ -27,22 +28,33 @@ def index(request):
 
         sparql = SPARQLWrapper("http://semtech.mty.itesm.mx:3030/Fototeca/sparql")
         sparql.setQuery("""
-                    SELECT ?property ?object
-                    WHERE {""" +
-                        uri + """ ?property ?object  .
+                    SELECT (count(?object) as ?relations) ?foto2
+                    WHERE { <""" + uri + """>
+                       ?property1 ?object  .
+                      ?foto2 ?property2 ?object .
+                      filter(?property1 = ?property2)
+                      ?foto2 a <http://semtech.mty.itesm.mx:8888/marmotta/resource/fototeca/Picture> .
+                      filter(?property1 != <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>)
+                      filter(?foto2 != <""" + uri + """>)
                     }
+                    group by ?foto2
+                    ORDER BY DESC(?relations)
+                    LIMIT 10
                 """)
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
         concepts = []
-        qproperties = []
         qobjects = []
-        arreglo = [10,20,30,40,50]
+        qrelations = []
+
+
+
+
         for result in results["results"]["bindings"]:
-            #print result["property"]['value'], result["object"]['value']
-            qproperties.append(result["property"]['value'])
-            qobjects.append(result["object"]['value'])
-        context_dict = {'query_properties': json.dumps(qproperties), 'query_objects': json.dumps(qobjects)}
+            #print result["property1"]['value'], result["relations"]['value']
+            qobjects.append(result["foto2"]['value'])
+            qrelations.append(result["relations"]['value'])
+        context_dict = { 'query_objects': json.dumps(qobjects), 'query_relations': json.dumps(qrelations), 'uri_view': uri, 'resultados': json.dumps(results["results"]["bindings"])}
 
         # return HttpResponseRedirect('/semanticquery/search_query_results/')
         return render(request, 'semanticquery/search_query_results.html', context_dict)
@@ -72,5 +84,3 @@ def search_query(request):
 #def search_query_results(request):
  #   context_dict = {'name': "Gil"}
   #  return render(request, 'semanticquery/search_query_results.html', context_dict)
-
-
